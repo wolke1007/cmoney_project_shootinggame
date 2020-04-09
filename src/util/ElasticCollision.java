d8f65c7e4c0ce2824a0b62550169c31b52948e43
chifengtsai <positive811025@gmail.com>
2020-04-09 11:45:04 +0800

    delete Enemy & Ammo single update fix Angle caculation bug & add normalAngle

--- src/util/ElasticCollision.java
+++ src/util/ElasticCollision.java
@@ -5,12 +5,81 @@
  */
 package util;
 
+import gameobj.GameObject;
+import graph.Circle;
 import graph.Graph;
+import graph.Rect;
+import java.util.LinkedList;
 
 /**
  *
  * @author F-NB
  */
 public class ElasticCollision {
-    private Graph self;
+
+    private Graph self;//想要移動的碰撞體
+    private float dx;//給予 x 的移動向量
+    private float dy;//給予 y 的移動向量
+    private LinkedList<GameObject> allObjects;
+    
+    private Angle angle;
+    private float divisor;//細分預判的等份數 //暫時不一定用到
+
+    public ElasticCollision(Graph self, float dx, float dy, LinkedList<GameObject> allObjects) {
+        setSelf(self);
+        setDXY(dx, dy);
+        setAllObjects(allObjects);
+        this.setDivisor(100);
+    }
+
+    public void setSelf(Graph self) {
+        if (this.self == null) {
+            if (self instanceof Circle) {
+                Circle tmp = (Circle) self;
+                this.self = new Circle(tmp.left(), tmp.top(), tmp.right(), tmp.bottom(), tmp.r());
+                return;
+            }
+            this.self = new Rect(self.left(), self.top(), self.right(), self.bottom());
+            return;
+        }
+        this.self.setLeft(self.left());
+        this.self.setTop(self.top());
+        this.self.setRight(self.right());
+        this.self.setBottom(self.bottom());
+    }
+
+    public void setDX(float dx) {
+        this.dx = dx;
+    }
+
+    public void setDY(float dy) {
+        this.dy = dy;
+    }
+
+    public void setDXY(float dx, float dy) {
+        setDX(dx);
+        setDY(dy);
+    }
+
+    public void setAllObjects(LinkedList<GameObject> allObjects) {
+        this.allObjects = allObjects;
+    }
+
+    public void setDivisor(float divisor) {
+        this.divisor = divisor;
+    }
+
+    public void newSet(Graph self, float dx, float dy, LinkedList<GameObject> allObjects) {
+        setSelf(self);
+        setDXY(dx, dy);
+        setAllObjects(allObjects);
+    }
+
+    private void offsetDX() {
+        float tmp = this.dx / this.divisor;//等分的距離
+        float go = 0;
+        for(int i = 0; i < this.divisor; i++){
+            
+        }
+    }
 }
